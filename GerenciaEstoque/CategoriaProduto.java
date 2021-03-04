83d0f11cf697e69fd5da7d87ab009deeb9a93600
Ketrin Vargas <ketrin_vargas@outlook.com>
2021-03-04 15:45:46 -0300

    Update CategoriaProduto

--- GerenciaEstoque/CategoriaProduto.java
+++ GerenciaEstoque/CategoriaProduto.java
@@ -1,5 +1,29 @@
 package GerenciaEstoque;
 
 public class CategoriaProduto {
-    
+
+    private int prodUnidade;
+    private float prodQuilo;
+
+    public CategoriaProduto(int prodUnidade, float prodQuilo) {
+        this.prodUnidade = prodUnidade;
+        this.prodQuilo = prodQuilo;
+    }
+
+    public int getPordUnidade() {
+        return prodUnidade;
+    }
+
+    public float getProdQuilo() {
+        return prodQuilo;
+    }
+
+    public void setProdUnidade(int prodUnidade) {
+        this.prodUnidade = prodUnidade;
+    }
+
+    public void setProdQuilo(float prodQuilo) {
+        this.prodQuilo = prodQuilo;
+    }
+
 }
