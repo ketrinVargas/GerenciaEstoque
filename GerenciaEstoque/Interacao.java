2b0f2decd0e309728af372e72dec7e995b959a01
Rafael Bernardo de Souza <rafael.md1982@gmail.com>
2021-03-04 22:46:14 -0300

    changes

--- GerenciaEstoque/Interacao.java
+++ GerenciaEstoque/Interacao.java
@@ -2,7 +2,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class App {
+import org.graalvm.compiler.nodes.NodeView.Default;
+
+public class Interacao {
     Scanner entrada = new Scanner(System.in);
 
     public static void main(String[] args) {
@@ -10,12 +12,20 @@ public class App {
         do {
             escolhaValida  = primeiraEscolha(0);
         } while (escolhaValida);
-        
-
-        
     }   
 
-    private boolean primeiraEscolha(int e) {
+    public void mostar(String string) {
+        System.out.println(string);
+    }
+
+
+    public String obtemDoUsuario(String texto) {
+        mostar("Digite " + texto + " :  /n");
+        String digitado = entrada.nextLine();
+        return digitado;        
+    }
+
+    public boolean primeiraEscolha(int e) {
         switch (e) {
             case 0: 
                 mostar( "Escolha a opção: \n" + 
@@ -91,17 +101,6 @@ public class App {
         }
     }
 
-    private void mostar(String string) {
-        System.out.println(string);
-    }
-
-
-    private String obtemDoUsuario(String texto) {
-        mostar("Digite " + texto + " :  /n");
-        String digitado = entrada.nextLine();
-        return digitado;        
-    }
-
     private void cadastrarProduto() {      
         String nome = obtemDoUsuario("o  nome do produto");
         String descricao = obtemDoUsuario("a descricao do produto");
@@ -111,13 +110,43 @@ public class App {
         addProduto(produto);
     }
 
-    private void consultarProduto() {
+    private int consultarProduto() {
         int codigo = Integer.parseInt(obtemDoUsuario("o codigo do produto"));
         Produto produto = getProduto(codigo);
         mostar(produto);
+        return codigo;
     }
 
-    private void alterarProduto() {
+    private void alterarProduto() { 
+        int codigo = consultarProduto();
+        double novoValor;
+        int escolha = Integer.parseInt(obtemDoUsuario(
+            "o numero referente ao oq vc quer alterar." +
+            "1- Atualizar Quantidade" +
+            "2- Atualizar preço" +
+            "3- Aumentar quantidade" +
+            "4- Diminuir quantidade"));
+        switch (escolha) {
+            case 1:
+                novoValor = Double.parseDouble(obtemDoUsuario("o novo valor da Quantidade"));
+                updateQuantidade(codigo , novoValor);
+                break;
+            case 2:
+                novoValor = Double.parseDouble(obtemDoUsuario("o novo valor do Preço"));
+                updatePreco(codigo, novoValor);
+                break;
+            case 3:
+                novoValor = Double.parseDouble(obtemDoUsuario("o valor a aumentar"));
+                addQuantidade(codigo, novoValor);
+                break;
+            case 4:
+                novoValor = Double.parseDouble(obtemDoUsuario("o valor a diminuir."
+                ));
+                subQuantidade(codigo, novoValor);
+                break;        
+            default: 
+                break;
+        }
         
     }
 
@@ -141,6 +170,8 @@ public class App {
     private void totalDoDia() {
         
     }
+
+
 
     
 }
